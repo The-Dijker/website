61b37793e975ee87347626e3d45a58234127244c
Rutger Pronk <rutger.pronk11@gmail.com>
2024-12-08 03:14:04 +0100

    Remove temp select

--- src/components/i18n/LanguageSwitcher.tsx
+++ src/components/i18n/LanguageSwitcher.tsx
@@ -24,28 +24,16 @@ export function LocaleSwitcher() {
   }
 
   return (
-    <>
-      <select
-        className="rounded-md border-none border-primary bg-primary bg-transparent p-2 text-white outline-none ring-primary focus:ring-2 focus:ring-primary"
-        defaultValue={locale}
-        onChange={onSelectChange}
-      >
-        {routing.locales.map((locale) => (
-          <option className={"text-white"} key={locale} value={locale}>
-            {t(locale)}
-          </option>
-        ))}
-      </select>
-      <select
-        id="countries"
-        className="bg-gray-50 border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 block w-full rounded-lg border p-2.5 text-sm dark:text-white"
-      >
-        <option selected>Choose a country</option>
-        <option value="US">United States</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
-      </select>
-    </>
+    <select
+      className="rounded-md border-none border-primary bg-primary bg-transparent p-2 text-white outline-none ring-primary focus:ring-2 focus:ring-primary"
+      defaultValue={locale}
+      onChange={onSelectChange}
+    >
+      {routing.locales.map((locale) => (
+        <option className={"text-white"} key={locale} value={locale}>
+          {t(locale)}
+        </option>
+      ))}
+    </select>
   );
 }
