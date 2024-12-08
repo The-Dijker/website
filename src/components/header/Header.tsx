0247ee659d9433bea442676d87a55339ac77c1c9
Rutger Pronk <rutger.pronk11@gmail.com>
2024-12-08 02:06:10 +0100

    Make header responsive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/routing";
+import { Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import clsx from "clsx";
 
 export function Header() {
   const t = useTranslations("header");
+  const [opened, { toggle, close }] = useDisclosure(false);
 
   const navItems = [
     { href: "/", label: t("home") },
@@ -14,13 +20,14 @@ export function Header() {
   ];
 
   return (
-    <header className={"bg-primary flex justify-between items-center px-12"}>
-      <Image src={"/logo.webp"} alt={"Logo"} width={214} height={80} />
+    <header className="relative z-10 flex items-center justify-between bg-primary px-4 md:px-12">
+      <Image src="/logo.webp" alt="Logo" width={214} height={80} />
 
-      <nav className={"flex"}>
+      {/* Desktop Navigation */}
+      <nav className="hidden md:flex">
         {navItems.map((item) => (
           <Link
-            className={"text-white p-4 hover:opacity-80 font-semibold"}
+            className="p-4 font-semibold text-white hover:opacity-80"
             key={item.href}
             href={item.href}
           >
@@ -28,6 +35,37 @@ export function Header() {
           </Link>
         ))}
       </nav>
+
+      {/* Mobile Navigation */}
+      <Burger
+        opened={opened}
+        onClick={toggle}
+        color="white"
+        className="md:hidden"
+      />
+
+      <div
+        className={clsx(
+          "absolute left-0 top-0 h-screen w-screen bg-primary",
+          "-z-10 transition-all duration-100 ease-in-out",
+          opened
+            ? "translate-y-20 opacity-100"
+            : "pointer-events-none translate-y-16 opacity-0",
+        )}
+      >
+        <div className="flex min-h-screen flex-col bg-primary">
+          {navItems.map((item) => (
+            <Link
+              className="p-6 text-center text-2xl font-semibold text-white hover:opacity-80"
+              key={item.href}
+              href={item.href}
+              onClick={close}
+            >
+              {item.label}
+            </Link>
+          ))}
+        </div>
+      </div>
     </header>
   );
 }
