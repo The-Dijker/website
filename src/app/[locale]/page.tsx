cc68ea100ae016a97dd3ae1c39d5ac9194244994
Rutger Pronk <rutger.pronk11@gmail.com>
2024-12-09 19:26:15 +0100

    Add metadata

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -15,10 +15,7 @@ export default function HomePage() {
         height="315"
         src="https://www.youtube.com/embed/m3ZI0HB3BCs?si=daGCnNEEwgGR0P8u"
         title="YouTube video player"
-        // frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        // referrerpolicy="strict-origin-when-cross-origin"
-        // allowfullscreen
       />
 
       <Image
