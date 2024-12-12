7620a3e136fe8f233ca49e8a8456b22b9dbd1cd7
Rutger Pronk <rutger.pronk11@gmail.com>
2024-12-12 16:22:24 +0100

    Add feature section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,16 @@
 import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/routing";
+import { Features } from "@/components/Features/Features";
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
   return (
-    <div>
-      <h1>{t("title")}</h1>
-      <Link href="/about">{t("about")}</Link>
-    </div>
+    <main className={"mx-auto w-full max-w-screen-2xl space-y-20 p-20"}>
+      <div>
+        <h1>{t("title")}</h1>
+        <Link href="/about">{t("about")}</Link>
+      </div>
+      <Features />
+    </main>
   );
 }
